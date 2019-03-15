475e5c9668a04d699b6dbeefbdb2eca850ec61f1
nwkeith <structor125@gmail.com>
2019-03-14 22:22:24 -0600

    Homework 18 for Python. Now on Github!

---
+++ HW18.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Task 1
+df=pd.read_excel('data_tasks.xlsx', sheet_name='task1')
+print(df)
+print(df.values[:,1])
+time = df.values[:,0]
+temp=df.values[:,1]
+stdev=df.values[:,2]
+#error=(1.96*stdev)/ # I would use the 95% CI but I don't know n, so I'll jsut assume the standard deviation is my error
+line,caps,bars=plt.errorbar(
+        time,
+        temp,
+        stdev,
+        fmt="gs--",
+        linewidth=2,
+        elinewidth=.5,
+        ecolor="k",
+        capsize=5,
+        capthick=1
+        )
+plt.xticks(time)
+plt.yticks([0,5,10,15,20,25])
+plt.legend(["Standard Deviation"], numpoints=1, loc=('upper left'))
+plt.title("Moss Temperature in Spring", fontsize=24)
+plt.xlabel("Time in minutes", fontsize=14)
+plt.ylabel("Temperture in degrees C", fontsize=14)
+plt.savefig('mossTemp.png', dpi=600)
+plt.show()
+
+# Task 2
+t2=pd.read_excel('data_tasks.xlsx', sheet_name='task2')
+print(t2)
+time=t2.values[1:,0]
+vegasC=t2.values[1:,1]
+vegasS=t2.values[1:,2]
+durangoC=t2.values[1:,3]
+durangoS=t2.values[1:,4]
+denverC=t2.values[1:,5]
+denverS=t2.values[1:,6]
+barWidth=.2
+
+r1=np.arange(len(vegasC))
+r2=[i+barWidth for i in r1]
+r3=[l+(2*barWidth) for l in r1]
+print(vegasC)
+plt.bar(r1, vegasC, color="#7f6d5f", width=barWidth, edgecolor='white', label='Las Vegas', yerr = vegasS, capsize=5)
+plt.bar(r2, durangoC, color="blue", width=barWidth, edgecolor='white', label='Durango', yerr=durangoS, capsize=5)
+plt.bar(r3, denverC, color="red", width=barWidth, edgecolor='white', label='Denver', yerr=denverS, capsize=5)
+plt.title("City Tempertures", fontsize=24)
+plt.legend(loc= "upper left")
+plt.xlabel('Hour', fontsize=12, fontweight="bold")
+plt.ylabel('Temperature in degrees C', fontsize=12, fontweight="bold")
+plt.xticks([r+barWidth for r in range(len(vegasC))], ['1','2','3','4','5','6'])
+plt.savefig('cityTemp.png', dpi=600)
+plt.show()
